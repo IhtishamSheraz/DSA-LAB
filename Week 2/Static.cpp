430747aabd91c88432b5a4ee913206159f784f87
IhtishamSheraz <ihtishamsheraz000@gmail.com>
2025-05-27 20:57:09 +0500

    Static.cpp

--- Week 2/Static.cpp
+++ Week 2/Static.cpp
@@ -1,231 +1,149 @@
 //EXAMPLE 2.1:
 //#include <iostream>
+//#include <cstdlib>
 //#include "stack1.h"
-//#include <string>
 //
 //using namespace std;
 //
-//int main() {
-//    stack s, s2;
-//    string stri;
-//    char ch;
-//    for (int i = 0; i < 20; i++) {
-//        s.Push(i);
-//    }
-//    while (!s.IsEmpty()) {
-//        int item;
-//        s.Pop(item);
-//        cout << item << " ";
-//    }
-//    cout << endl;
-//    return 0;
+//stack::stack() {
+//	top = -1;
 //}
-// EXERCISE 2.1:
-// #include <iostream>
+//int stack::IsEmpty() const {
+//	return (top == -1);
+//}
+//
+//int stack::IsFull() const {
+//	return (top == Max_items - 1);
+//}
+//
+//void stack::Push(itemtype newitem) {
+//	if (IsFull()) {
+//		cout << "Stack overflow!" << endl;
+//		exit(1);
+//	}
+//	top++;
+//	items[top] = newitem;
+//}
+//void stack::Pop(itemtype& newitem) {
+//	if (IsEmpty()) {
+//		cout << "Stack underflow!" << endl;
+//		exit(1);
+//	}
+//	newitem = items[top];
+//	top--;
+//}
+//
+//EXERCISE 2.1:
+//#include <iostream>
+//#include <cstdlib>
 //#include "stack1.h"
-//#include <string>
 //
 //using namespace std;
 //
-//int main() {
-//    stack s, s2;
-//    string stri;
-//    char ch;
+//stack::stack() {
+//	top = -1;
+//}
+//int stack::IsEmpty() const {
+//	return (top == -1);
+//}
 //
-//   
+//int stack::IsFull() const {
+//	return (top == Max_items - 1);
+//}
 //
-//    for (int i = 0; i < 20; i++) {
-//        s.Push(i);
-//    }
-//   
-//    cout << "Top element: " << s.Peek() << endl;
+//void stack::Push(itemtype newitem) {
+//	if (IsFull()) {
+//		cout << "Stack overflow!" << endl;
+//		exit(1);
+//	}
+//	top++;
+//	items[top] = newitem;
+//}
 //
-//    
-//    while (!s.IsEmpty()) {
-//        int item;
-//        s.Pop(item);
-//        cout << item << " ";
-//    }
-//    cout << endl;
+//void stack::Pop(itemtype& newitem) {
+//	if (IsEmpty()) {
+//		cout << "Stack underflow!" << endl;
+//		exit(1);
+//	}
+//	newitem = items[top];
+//	top--;
+//}
 //
-//    return 0;
+//itemtype stack::Peek() const {
+//	if (IsEmpty()) {
+//		cout << "Stack is empty, cannot peek!" << endl;
+//		exit(1);
+//	}
+//	return items[top];
 //}
 //EXERCISE 2.2:
-//#include <iostream>
-//#include <string>
 //#include "stack1.h"
 //
-//using namespace std;
-//
-//string reverse(string str) {
-//    stack s;
-//
-//    for (char ch : str) {
-//        s.Push(ch);
-//    }
-//
-//   
-//    string rev = "";
-//    while (!s.IsEmpty()) {
-//        char ch;
-//        s.Pop(ch);
-//        rev += ch;
-//    }
-//
-//    return rev;
+//stack::stack() {
+//    top = -1;
 //}
 //
-//int main() {
-//    string stri;
-//    cout << "Enter a string: ";
-//    getline(cin, stri);
+//int stack::IsEmpty() const {
+//    return (top == -1);
+//}
 //
-//    string rev = reverse(stri);
-//    cout << "reversed string: " << rev << endl;
+//int stack::IsFull() const {
+//    return (top == Max_items - 1);
+//}
 //
-//    return 0;
+//void stack::Push(char ch) {
+//    if (IsFull()) {
+//        cout << "Stack overflow!" << endl;
+//        return;
+//    }
+//    items[++top] = ch;
+//}
+//
+//void stack::Pop(char& ch) {
+//    if (IsEmpty()) {
+//        cout << "Stack underflow!" << endl;
+//        return;
+//    }
+//    ch = items[top--];
 //}
 //EXERCISE 2.3:
-//#include <iostream>
-//#include <cctype>
+
 //#include "stack1.h"
 //
-//using namespace std;
-//
-//
-//bool isvalid(string expr) {
-//    Stack s;
-//    for (char ch : expr) {
-//        if (ch == '(' || ch == '{' || ch == '[') {
-//            s.push(ch);
-//        }
-//        else if (ch == ')' || ch == '}' || ch == ']') {
-//            if (s.isEmpty()) return false;
-//            char top = s.pop();
-//            if ((ch == ')' && top != '(') ||
-//                (ch == '}' && top != '{') ||
-//                (ch == ']' && top != '[')) {
-//                return false;
-//            }
-//        }
-//    }
-//    return s.isEmpty();
+//Stack::Stack() {
+//    top = -1;
 //}
 //
 //
-//int precedencerule(char op) {
-//    if (op == '+' || op == '-') return 1;
-//    if (op == '*' || op == '/') return 2;
-//    return 0;
+//bool Stack::isEmpty() {
+//    return top == -1;
+//}
+//bool Stack::isFull() {
+//    return top == MAX_SIZE - 1;
+//}
+//
+//void Stack::push(char value) {
+//    if (!isFull()) {
+//        arr[++top] = value;
+//    }
+//    else {
+//        cout << "Stack Overflow\n";
+//    }
+//}
+//char Stack::pop() {
+//    if (!isEmpty()) {
+//        return arr[top--];
+//    }
+//    else {
+//        cout << "Stack Underflow\n";
+//        return '\0';
+//    }
 //}
 //
 //
-//string infixtopostfix(string infix) {
-//    Stack s;
-//    string postfix = "";
-//
-//    for (char ch : infix) {
-//        if (isalnum(ch)) {
-//            postfix = postfix+ ch;
-//        }
-//        else if (ch == '(') {
-//            s.push(ch);
-//        }
-//        else if (ch == ')') {
-//            while (!s.isEmpty() && s.peek() != '(') {
-//                postfix += s.pop();
-//            }
-//            s.pop();  
-//        }
-//        else {  
-//            while (!s.isEmpty() && precedencerule(s.peek()) >= precedencerule(ch)) {
-//                postfix += s.pop();
-//            }
-//            s.push(ch);
-//        }
+//char Stack::peek() {
+//    if (!isEmpty()) {
+//        return arr[top];
 //    }
-//
-//    while (!s.isEmpty()) {
-//        postfix += s.pop();
-//    }
-//
-//    return postfix;
-//}
-//
-//
-//int checkpostfix(string postfix) {
-//    Stack s;
-//
-//    for (char ch : postfix) {
-//        if (isdigit(ch)) {
-//            s.push(ch - '0');  
-//        }
-//        else {
-//            int val2 = s.pop();
-//            int val1 = s.pop();
-//
-//            switch (ch) {
-//            case '+': s.push(val1 + val2); break;
-//            case '-': s.push(val1 - val2); break;
-//            case '*': s.push(val1 * val2); break;
-//            case '/':
-//                if (val2 == 0) { 
-//                    cout << "Error: Division by zero!" << endl;
-//                    return 0;  
-//                }
-//                s.push(val1 / val2);
-//                break;
-//            default:
-//                cout << "Invalid operator: " << ch << endl;
-//                return 0;
-//            }
-//        }
-//    }
-//
-//    return s.pop();
-//}
-//
-//
-//int main() {
-//    string exp;
-//    int choice;
-//
-//    do {
-//        cout << "\nplease select ur choice:\n";
-//        cout << "1 Check Parenthesis Validity\n";
-//        cout << "2 Convert Infix to Postfix\n";
-//        cout << "3 Evaluate Postfix exp\n";
-//        cout << "4 Exit\n";
-//        cout << "Enter choice: ";
-//        cin >> choice;
-//        if (choice == 1) {
-//            cout << "enetr expression :";
-//            cin >> exp;
-//            cout << (isvalid(exp) ? "Valid" : "Invalid") << endl;
-//            break;
-//        }
-//        else if (choice == 2) {
-//            cout << "Enter infix exp: ";
-//            cin >> exp;
-//            cout << "Postfix: " << infixtopostfix(exp) << endl;
-//            break;
-//
-//        }
-//        else if (choice == 3) {
-//            cout << "Enter postfix exp: ";
-//            cin >> exp;
-//            cout << "Result: " << checkpostfix(exp) << endl;
-//            break;
-//
-//        }
-//        else {
-//            cout << "stop";
-//            break;
-//           
-//        }
-//       
-//        cin >> choice;
-//    } while (choice == !4);
-//
-//    return 0;
+//    return '\0';
 //}
